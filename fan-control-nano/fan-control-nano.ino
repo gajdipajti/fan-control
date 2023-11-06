8f8518c9da7f4966474427eafa65b761824da6f0
Gajdos Tamás <gajdipajti@gmail.com>
2023-11-06 17:16:04 +0100

    Batch of Arduino updates
    I continued the work on the code and on the circuit on the prototype board. Some functions were
    removed or updated. The current idea is to use more arrays and access them by index. At the
    current stage I do not want to use advanced techniques, as we have enough space and memory.
    Basically the serial communication part is complete, only the munin part is left.
    
    Itemized:
    
    * General configuration string and bool for enabling and disabling a fan channel.
    * The fan pwm pins are now defined in a single array.
    * The settings are defined in a 5*8 2D array:
      * manual [0,1]
      * current pwm value [0-255]
      * pwm values [LOW[0-255], HIGH[0-255], CRITICAL[255]]
      * temp values [tLOW[0-85], tHIGH[0-85], tCRITICAL[85]]
    * start interrupt work.
    * set ADC reference to 1100 mV, this increases the resolution of the LM35* sensor.
    * temperature source wrapper.
    * all serial commands.
    * automode: read temperature and update fan pwm speeds accordingly.
    
    NOTE: I used the map() and constrain() functions to convert or limit ranges.
    
    It compiles in old (1.8.19) and new (2.2.1) Arduino IDE. Arduino Nano.
    
    Sketch uses 10460 bytes (34%) of program storage space. Maximum is 30720 bytes.
    Global variables use 504 bytes (24%) of dynamic memory, leaving 1544 bytes for local variables. Maximum is 2048 bytes.

--- fan-control-nano/fan-control-nano.ino
+++ fan-control-nano/fan-control-nano.ino
@@ -39,20 +39,20 @@ Serial Commands:
 
   Main Functions:
    * fan?             - GET installed fans, all information
-   * fan[A-F]?        - GET information about a single fan channel
    * pwm?             - GET all PWM outputs
    * pwm[A-F]?        - GET PWM output
+   * pwm[A-F]p        - SET PWM output to pilot mode
    * pwm[A-F][0-255]  - SET PWM output manually
-   * t?               - GET all measured temperature
-   * tA?              - GET measured temperature
+   * rpm[C,D]?        - GET RPM output from 4-wire fans
+   * t?               - GET measured temperature
+   * t[D,L]?          - GET measured temperature from selected source
    * ts?              - GET temperature sources (example Dallas 1-wire or LM35)
 
   Optional Functions: [when extra circuit is present]
    * lcd[?,0-4] - GET/SET LCD Backlight; Update LCD
-   * heart[0-1] - turn hearbeat LED ON/OFF
   Calibration Functions:
-   * Ch[L,H][A-F]?        - GET LOW HIGH temperatue for a channel
-   * Ch[L,H][A-F][0-100]  - SET LOW HIGH temperatue for a channel
+   * Ch[L,H,C][A-F]?        - GET LOW HIGH temperatue for a channel
+   * Ch[L,H,C][A-F][0-100]  - SET LOW HIGH temperatue for a channel
    * p[L,H][A-F]?         - GET LOW HIGH PWM speed for a channel
    * p[L,H][A-F][0-255]   - SET LOW HIGH PWM for a channel
   Misc Func:
@@ -68,26 +68,43 @@ Serial Commands:
 #include <uptime.h>
 #include <OneWire.h>
 // #include <LiquidCrystal_I2C.h>
+// LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+// Fan configuration - change this
+String fanconfiguration = "5ABCDE";
+const bool fanmask[5] = {true, true, true, true, true};
 // Variables and constants which might be checked in the GUI
 const byte  swVersion = 001;
 const float hwVersion = 1.0;
 const char  boardSubVersion = 'A';
 float time;
 
-// Define PWM pins
-const byte pwmA = 5;
-const byte pwmB = 6;
-const byte pwmC = 9;
-const byte pwmD = 10;
-const byte pwmE = 11;
+// Define PWM pins Channel A-E
+const byte pwm[5] = {5, 6, 9, 10, 11};
 // const byte pwmF = 3;     // Used as interrupt
+
+// Define 2D settings array for pwm and temperature
+// ROWS: Channel A-E
+// COLS: manual[0-1], current[0-255], LOW[0-255], HIGH[0-255], CRITICAL[255], ...
+//                                         tLOW[0-85], tHIGH[0-85], tCRITICAL[85]
+byte preset[5][8] = {
+  {0, 128, 30, 200, 255, 15, 60, 85},
+  {0, 128, 30, 200, 255, 15, 60, 85},
+  {0, 128, 30, 200, 255, 15, 60, 85},
+  {0, 128, 30, 200, 255, 15, 60, 85},
+  {0, 128, 30, 200, 255, 15, 60, 85}
+};
 
 // Define interrupt pins
 const byte intC = 2;
 const byte intD = 3;
 
+// Store rpm values in vol
+volatile unsigned int rpmC = 0;
+volatile unsigned int rpmD = 0;
+
 // Define temperature and lcd pins
+char tempSource = 'L';      // The source of the temperature: 'L' - LM35, 'D' - DS18B20, ...
 const byte lm35 = A7;       // LM35 connected
 const byte ds18 = 12;       // DS18B20 sensor is connected
 OneWire  ds(ds18);
@@ -102,7 +119,7 @@ OneWire  ds(ds18);
 // For serial communication.
 String inputString = "";        // a string to hold incoming data
 bool stringComplete = false;    // whether the string is complete
-bool streamMode = false;        // enable stream mode
+bool automode = true;           // enable auto mode
 
 // Wait mode
 short waitPeriod = 1000;
@@ -117,19 +134,20 @@ void setup() {  // The initial setup, that will run every time when we connect t
   
   // Voltage channel outputs, it's not needed for analogWrite, but just in case.
   // https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/
-  pinMode(pwmA, OUTPUT); analogWrite(pwmA, LOW);
-  pinMode(pwmB, OUTPUT); analogWrite(pwmB, LOW);
-  pinMode(pwmC, OUTPUT); analogWrite(pwmC, LOW);
-  pinMode(pwmD, OUTPUT); analogWrite(pwmD, LOW);
-  pinMode(pwmE, OUTPUT); analogWrite(pwmE, LOW);
+  pinMode(pwm[0], OUTPUT); analogWrite(pwm[0], LOW);
+  pinMode(pwm[1], OUTPUT); analogWrite(pwm[1], LOW);
+  pinMode(pwm[2], OUTPUT); analogWrite(pwm[2], LOW);
+  pinMode(pwm[3], OUTPUT); analogWrite(pwm[3], LOW);
+  pinMode(pwm[4], OUTPUT); analogWrite(pwm[4], LOW);
   // pinMode(pwmF, OUTPUT); analogWrite(pwmF, LOW);  // Not used
 
   // Temperature input
+  analogReference(INTERNAL);    // set AREF to 1.1 V
   pinMode(lm35, INPUT);
 
   // Copied the relevant part from here: http://playground.arduino.cc/Code/PwmFrequency
-  TCCR1B = TCCR1B & 0b11111000 | 0x01;    // Change PWM Frequency for Timer2.  f=~31kHz
-  // TCCR2B = TCCR2B & 0b11111000 | 0x01;    // Change PWM Frequency for Timer1.  f=~31kHz
+  TCCR1B = TCCR1B & 0b11111000 | 0x01;    // Change PWM Frequency for Timer1.  f=~31kHz
+  // TCCR2B = TCCR2B & 0b11111000 | 0x01;    // Change PWM Frequency for Timer2.  f=~31kHz
 
   delay(1000);
   
@@ -138,16 +156,189 @@ void setup() {  // The initial setup, that will run every time when we connect t
 }
 
 float lm35Temp() {
-  // LM35DT: 10mV/°C -> (adc*(aref/1024))/10
-  // Resolution: 0,49°C; Precision +/- 1,5°C
-  // Range: 0°C - 100°C [0 - 205 ADC]
+  // LM35DZ: 10mV/°C -> (adc*(aref/1024))/10
+  // Current settings for AREF = 1100 mV
+  // Resolution: 0,11°C; Precision +/- 1 °C
+  // Range: 0°C - 110°C [0 - 1023 ADC]
   float tmV = analogRead(lm35);
-  return tmV*(500.00/1024.00);
+  return tmV*(110.00/1024.00);
 }
+
+float ds18Temp() {
+  // TODO
+  return 0.00;
+}
+
+float getTemperature(char Source) {
+  // NOTE: Implement here other sources.
+  switch (Source) {
+    case 'L':
+      return lm35Temp(); break;
+    case 'D':
+      return ds18Temp(); break;
+    default:
+      break;
+  }
+}
+
 void loop() {
   if (stringComplete) {
+    if (inputString.startsWith("t")) {
+      switch(inputString.charAt(1)) {
+        case '?':
+          Serial.println(getTemperature(tempSource)); break;
+        case 's':
+          Serial.println(tempSource); break;  // Display source
+        case 'L':
+          Serial.println(lm35Temp()); break;  // Only for debugging
+        case 'D':
+          Serial.println(ds18Temp()); break;  // Only for debugging
+        default:
+          Serial.print("Syntax Error: "); Serial.println(inputString);
+          break;
+      }
+    } else if (inputString.startsWith("pwm")) {
+      if (inputString.endsWith("?")) {
+        // Read current pwm settings from array.
+        switch(inputString.charAt(3)) {
+          case 'A':
+            Serial.println(preset[0][1]); break;
+          case 'B':
+            Serial.println(preset[1][1]); break;
+          case 'C':
+            Serial.println(preset[2][1]); break;
+          case 'D':
+            Serial.println(preset[3][1]); break;
+          case 'E':
+            Serial.println(preset[4][1]); break;
+          case '?':
+            Serial.print(preset[0][1]); Serial.print('; ');
+            Serial.print(preset[1][1]); Serial.print('; ');
+            Serial.print(preset[2][1]); Serial.print('; ');
+            Serial.print(preset[3][1]); Serial.print('; ');
+            Serial.println(preset[4][1]); break;
+          default:
+            Serial.print("Syntax Error: "); Serial.println(inputString);
+            break;
+        }
+      } else if (inputString.endsWith("p")) {
+        // Set to pilot mode from manual.
+        int value = constrain(inputString.charAt(3),65,69)-65;
+        preset[value][0] = 0;
+        Serial.println("OK");
+      } else {
+        // Manually set the pwm value. Set to manual mode.
+        int value = constrain(inputString.charAt(3),65,69)-65;
+        preset[value][1] = constrain(byte(inputString.substring(4).toInt()),0,255);
+        preset[value][0] = 1;
+        analogWrite(pwm[value],inputString.substring(4).toInt());
+        Serial.println("OK");
+      }
+    } else if (inputString.startsWith("rpm")) {
+      switch(inputString.charAt(3)) {
+        case 'C':
+          Serial.println(rpmC); break;
+        case 'D':
+          Serial.println(rpmD); break;
+        case '?':
+          Serial.print(rpmC); Serial.print('; ');
+          Serial.println(rpmD); break;
+        default:
+          Serial.print("Syntax Error: "); Serial.println(inputString);
+          break;
+      } 
+    } else if (inputString.startsWith("fan?")) {
+      // Print information about the fans
+      Serial.println(fanconfiguration);
+    } else if (inputString.startsWith("?")) {
+      // Are you there?
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+      Serial.println("OK");
+    } else if (inputString.startsWith("hrs?")) {
+      // Uptime
+      uptime::calculateUptime();
+      Serial.print(uptime::getDays());      Serial.print(":");
+      Serial.print(uptime::getHours());     Serial.print(":");
+      Serial.print(uptime::getMinutes());   Serial.print(":");
+      Serial.println(uptime::getSeconds());
+
+    } else if (inputString.startsWith("cbn?")) { Serial.println(swVersion);  // Code Build Number
+    } else if (inputString.startsWith("ver?")) {
+      Serial.print(hwVersion, 1); Serial.println(boardSubVersion);            // Version Number + Board SubVersion
+    } else if (inputString.startsWith("p")) {
+      int value = constrain(inputString.charAt(2),65,69)-65;
+      if (inputString.endsWith("?")) {
+        // GET LOW or HIGH pwm settings.
+        switch(inputString.charAt(1)) {
+          case 'L':
+            Serial.println(preset[value][2]); break;
+          case 'M':
+            Serial.println(preset[value][3]); break;
+          default:
+            Serial.print("Syntax Error: "); Serial.println(inputString);
+            break;
+        }
+      } else {
+        // SET LOW or HIGH pwm settings.
+        // But constrain LOW between 0 and HIGH, and constrain HIGH between LOW and CRITICAL.
+        switch(inputString.charAt(1)) {
+          case 'L':
+            preset[value][2] = constrain(byte(inputString.substring(3).toInt()), 0, preset[value][3]);
+            Serial.print(preset[value][2]); 
+            Serial.println(" OK"); break;
+          case 'M':
+            preset[value][3] = constrain(byte(inputString.substring(3).toInt()), preset[value][2], preset[value][4]);
+            Serial.print(preset[value][3]); 
+            Serial.println(" OK"); break;
+          default:
+            Serial.print("Syntax Error: "); Serial.println(inputString);
+            break;
+        }     
+      }
+    } else if (inputString.startsWith("Ch")) {
+      int value = constrain(inputString.charAt(3),65,69)-65;
+      if (inputString.endsWith("?")) {
+        // GET LOW or HIGH pwm settings.
+        switch(inputString.charAt(2)) {
+          case 'L':
+            Serial.println(preset[value][5]); break;
+          case 'M':
+            Serial.println(preset[value][6]); break;
+          default:
+            Serial.print("Syntax Error: "); Serial.println(inputString);
+            break;
+        }
+      } else {
+        // SET LOW or HIGH pwm settings.
+        // But constrain LOW between 0 and HIGH, and constrain HIGH between LOW and CRITICAL.
+        switch(inputString.charAt(1)) {
+          case 'L':
+            preset[value][5] = constrain(byte(inputString.substring(4).toInt()), 0, preset[value][6]);
+            Serial.print(preset[value][5]); 
+            Serial.println(" OK"); break;
+          case 'M':
+            preset[value][3] = constrain(byte(inputString.substring(4).toInt()), preset[value][5], preset[value][7]);
+            Serial.println(preset[value][6]);
+            Serial.println(" OK"); break;
+          default:
+            Serial.print("Syntax Error: "); Serial.println(inputString);
+            break;
+        }     
+      }
+    }
     // Do your thing
-  } // There is no else
+  } else if (automode) {
+    // GET temperature in float, convert to int
+    // UPDATE pwm if not in manual
+    int currentTemp = int(getTemperature(tempSource));
+    for (int idx = 0; idx < fanconfiguration.charAt(0); idx++) {
+      if (fanmask[idx] && (preset[idx][0] == 0)) {
+        preset[idx][1] = map(currentTemp, preset[idx][5], preset[idx][6], preset[idx][2], preset[idx][3]);
+        analogWrite(pwm[idx], preset[idx][1]);
+      }
+    }
+  }
+  inputString = ""; stringComplete = false;      //clear the string
   //serialEvent();  // Workaround for ATTiny or ATMega chips without hardware serial
 }
 

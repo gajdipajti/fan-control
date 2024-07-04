d57e7d93d1d615b547d41f23b9967cdd5630328f
Gajdos Tamás <gajdipajti@gmail.com>
2024-07-04 09:40:07 +0200

    Constant has changed.

--- octave-codes/ntc.m
+++ octave-codes/ntc.m
@@ -1,12 +1,23 @@
 R25 = 4700;
-K=3850;
+K=3950;
 TC=0:0.1:120;
 T=TC+273;
 T25=25+273;
 RT=R25*e.^(K*(1./T-1./T25));
 
 figure;
+subplot(1,2,1);
 plot(TC,RT);
+hold on;
+line([0 25], [4700 4700], "color", "r");
+line([25 25], [0 4700], "color", "r");
+xlabel("Temperature °C");
+ylabel("Resistance [Ohm]");
+title("NTC resistance");
+hold off;
+
+subplot(1,2,2);
+semilogx(TC,RT);
 hold on;
 line([0 25], [4700 4700], "color", "r");
 line([25 25], [0 4700], "color", "r");
